3736702044bd1793b982596c54bd483ff6adedbb
Ihor Linnyk <ihor.linnyk@dev-pro.net>
2019-08-27 16:29:18 +0300

    tutor #2: creating layout, fetching first bunch of posts

---
+++ src/layout/Navbar.svelte
@@ -0,0 +1,17 @@
+<script>
+    import { Link } from 'svelte-routing';
+</script>
+
+<nav>
+    <div class="nav-wrapper">
+        <div class="container">
+            <div class="brand-logo right">
+                <Link to="/">Svelte Tutorial</Link>
+            </div>
+            <ul id="nav-mobile" class="left">
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+            </ul>
+        </div>
+    </div>
+</nav>
